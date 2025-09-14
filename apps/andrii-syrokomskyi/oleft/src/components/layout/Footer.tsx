892513faa7019aa198c2d34c22f2bfd0b8f10d73
Andrii Syrokomskyi <signmotion@gmail.com>
2025-09-14 22:25:56 +0200

    Implemented dynamic Footer.

--- apps/andrii-syrokomskyi/oleft/src/components/layout/Footer.tsx
+++ apps/andrii-syrokomskyi/oleft/src/components/layout/Footer.tsx
@@ -1,11 +1,15 @@
-const Footer = () => {
+interface Props {
+  site?: string;
+}
+
+const Footer = ({ site }: Props) => {
   return (
     <footer className="bg-appGray-100 pt-6 pb-[35px] md:pb-8 lg:pt-14">
       <div className="container">
         <div className="text-[#898C8A] leading-none font-medium tracking-[-0.41px] md:flex md:items-center md:justify-end">
           <div className="text-xs lg:text-base lg:leading-none">
-            {/* TODO: Get from BCMS footer entry. */}
-            &copy; {new Date().getFullYear()} oleft.market
+            &copy; {new Date().getFullYear()}
+            {site && ` ${site}`}
           </div>
         </div>
       </div>
