1e6b8f3751e7850bce8af2496aa3402b801f3fad
Andrii Syrokomskyi <signmotion@gmail.com>
2025-09-15 13:43:27 +0200

    Simplified the filter for articles. Removed unused components.

--- apps/andrii-syrokomskyi/oleft/src/components/articles/Filters.tsx
+++ apps/andrii-syrokomskyi/oleft/src/components/articles/Filters.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useState } from "react";
 import { defaultCountry, defaultLanguage } from "../../configure";
 import { getCountryName, getLanguageName } from "../../utils/localization";
-import { ArticlesDropdown } from "./Dropdown";
 import ArticlesSearchBar from "./Search";
 
 interface Props {
-  categories: string[];
   country?: string;
   language?: string;
 }
 
 const Filters = ({
-  categories,
   country = defaultCountry,
   language = defaultLanguage,
 }: Props) => {
   const [searchValue, setSearchValue] = useState("");
-  const [categoriesValue, setCategoriesValue] = useState("");
 
   const filterRedirect = (key: string, val: string) => {
     if (val) {
@@ -28,30 +24,19 @@ const Filters = ({
     if (searchValue) {
       filterRedirect("s", searchValue);
     }
-
-    if (categoriesValue) {
-      filterRedirect("c", categoriesValue);
-    }
-  }, [searchValue, categoriesValue, filterRedirect]);
+  }, [searchValue, filterRedirect]);
 
   return (
     <div className="pt-24 mb-8 md:pt-6 lg:pb-6 lg:border-b lg:border-[#F0F0F0] lg:mb-14">
       <div className="container">
-        <div className="relative z-10 grid grid-cols-2 gap-x-3 gap-y-[14px] lg:grid-cols-4 lg:gap-x-4">
+        <div className="relative z-10 gap-x-3 gap-y-[14px]">
           <ArticlesSearchBar
             static
             size="lg"
             country={country}
             language={language}
-            className="col-span-2"
+            className="w-full"
             onEnter={(value) => setSearchValue(value)}
-          />
-          <ArticlesDropdown
-            value={categoriesValue}
-            options={categories}
-            placeholder="Categories"
-            dropdownPosition="left"
-            onSelect={(value) => setCategoriesValue(value)}
           />
         </div>
       </div>
