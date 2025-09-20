b7f5f67c29b4b9142754779d9c58a2e5a4726950
Andrii Syrokomskyi <signmotion@gmail.com>
2025-09-21 01:09:23 +0200

    Reformatted.

--- packages/enhanced-article/src/index.ts
+++ packages/enhanced-article/src/index.ts
@@ -3,26 +3,20 @@
  */
 
 export {
-  EnhancedArticle,
-  
   ArticleStructure,
-  
+  type ArticleStructureType,
+  Code,
+  EnhancedArticle,
+  Guest,
+  MarkedList,
+  NumericList,
+  Paragraph,
+  Separator,
+  Table,
   Title1,
   Title2,
   Title3,
   Title4,
   Title5,
   Title6,
-  
-  Paragraph,
-  Table,
-  Code,
-  NumericList,
-  MarkedList,
-  Separator,
-  Guest,
-  
-  type ArticleStructureType,
-} from './enhanced-article';
-
-export { testEnhancedArticle } from './test-enhanced-article';
+} from "./enhanced-article";
